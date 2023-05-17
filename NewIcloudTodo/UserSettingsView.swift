4a93a55c9c5e2066b7cbdb9342741877abd1671d
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-05-17 22:34:22 +0200

    Add user settings view

---
+++ NewIcloudTodo/UserSettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  UserSettingsView.swift
+//  NewIcloudTodo
+//
+//  Created by Kaori Persson on 2023-05-17.
+//
+
+import SwiftUI
+
+struct UserSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserSettingsView()
+    }
+}
